6386ce490f951bca215656ebf2baece398eae1b1
TranBaNgoc <1612431@student.hcmus.edu.vn>
2019-10-14 10:04:56 +0700

    Tao project bang express-generator, to chuc thu muc, ket noi CSDL

---
+++ models/users.model.js
@@ -0,0 +1,18 @@
+var db = require('../utils/db')
+module.exports = {
+
+    // Get all
+    all: ()=>{
+        return db.load('SELECT * FROM user');
+    },
+
+    // Get by id
+    single: username => {
+        return db.load(`SELECT * FROM user WHERE username = '${username}'`);
+    },
+
+    // Insert
+    add: entity => {
+        return db.add(`user`, entity);
+    },    
+};
