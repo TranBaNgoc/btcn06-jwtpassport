f2436674dbb8f681233a85a6d71d3003985ce7b5
TranBaNgoc <tbngoc.khtn@gmail.com>
2019-10-23 19:33:19 +0700

    Update allow cross domain

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ var index = require("./routes/index");
 require('./passport');
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  
+  // allow options method work, ask experts for more
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({});
+  }
+  next();
+}
+app.use(allowCrossDomain);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
